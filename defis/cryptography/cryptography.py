93eba3c5f92fc863a0e163f928cd42e3dfa663d5
Habounou <barh.hugo@gmail.com>
2026-02-07 12:11:32 -0500

    Niveau 1-2 cryptography

--- defis/cryptography/cryptography.py
+++ defis/cryptography/cryptography.py
@@ -1,14 +1,62 @@
 with open("input_level_1.txt") as fichier:
-    input_1 = fichier.read()
+    input_1 = fichier.read().split()
+
+shift = 1
+resolu = ''
+for mot in input_1:
+    resolu += mot[-shift] + mot[:-shift]
+    if mot != input_1[-1]:
+        resolu += ' '
+print(resolu)
+
+
+with open("input_level_2.txt") as fichier:
+    input_2 = fichier.read().lower()
 
 for i in range(26):
+    print('==========' + str(i) + '==========')
     shift = i
     resolu = ''
-    for char in input_1:
-        if char != ' ':
-            resolu += chr(ord(char) + shift)
-        else:
+    for char in input_2:
+        if char == ' ':
             resolu += ' '
+        elif char == '-':
+            resolu += '-'
+        elif char == '\n':
+            resolu += '\n'
+        else:
+            nouvelle_ascii = (ord(char) + shift) % 123
+            if nouvelle_ascii < 97:
+                nouvelle_ascii += 97
+            resolu += chr(nouvelle_ascii)
+    resolu = resolu.capitalize()
+    print(resolu + '\n======================\n')
 
-    print(resolu)
 
+# with open("input_level_3.txt") as fichier:
+#     input_3 = fichier.read().lower()
+#
+# for i in range(26):
+#     print('==========' + str(i) + '==========')
+#     shift = i
+#     resolu = ''
+#     for char in input_3:
+#         if char == ' ':
+#             resolu += ' '
+#         elif char == '-':
+#             resolu += '-'
+#         elif char == '\n':
+#             resolu += '\n'
+#         elif char == '.':
+#             resolu += '.'
+#         elif char == ',':
+#             resolu += ','
+#         elif char == '_':
+#             resolu += '_'
+#         else:
+#             nouvelle_ascii = (ord(char) + shift) % 123
+#             if nouvelle_ascii < 97:
+#                 nouvelle_ascii += 97
+#             resolu += chr(nouvelle_ascii)
+#     resolu = resolu.capitalize()
+#     print(resolu + '\n======================\n')
